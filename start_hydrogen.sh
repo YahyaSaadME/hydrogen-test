b15201d786592bc6d1a350652f01f20b8b4e7c10
YahyaSaadME <yahyasaad.me@gmail.com>
2025-06-18 14:12:02 +0530

    service added

---
+++ start_hydrogen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change to the hydrogen directory
+cd /root/Desktop/hydrogen
+
+# Activate the virtual environment
+source /root/Desktop/.venv/bin/activate
+
+# Run your Python script (replace 'your_script.py' with your actual filename)
+python run_system.py
